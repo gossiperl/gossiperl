92eacd2bd553a0ea8a22319e1572d9f640fc957a
radekg <radek@gruchalski.com>
2015-01-17 01:11:25 +0100

    gossiperl_common covered by tests. #2

--- test/gossiperl_tests.erl
+++ test/gossiperl_tests.erl
@@ -2,7 +2,15 @@
 -include_lib("eunit/include/eunit.hrl").
 
 gossiperl_test_() ->
-  {setup, fun start/0, fun stop/1, [] }.
+  {setup, fun start/0, fun stop/1, [
+    fun test_get_iface_ip/0,
+    fun test_get_all_ipv4_addrs/0,
+    fun test_get_timestamp/0,
+    fun test_parse_binary_ip/0,
+    fun test_binary_join/0
+    ] }.
+
+-define(COMMON, gossiperl_common).
 
 start() ->
   Applications = [ asn1, crypto, public_key, erlsha2, jsx, thrift,
@@ -17,3 +25,41 @@ start() ->
 
 stop(_State) ->
   noreply.
+
+% Common
+
+test_get_iface_ip() ->
+  { ok, [ If | _ ] } = inet:getiflist(),
+  BinIf = list_to_binary( If ),
+  NotInterface = <<"NotAnInterface">>,
+  ?assertMatch({ error, no_iface }, ?COMMON:get_iface_ip( NotInterface )),
+  ?assertMatch({ ok, { _,_,_,_ } }, ?COMMON:get_iface_ip( BinIf )).
+
+test_get_all_ipv4_addrs() ->
+  ?assertMatch([ _ | _ ], ?COMMON:get_all_ipv4_addrs()).
+
+test_get_timestamp() ->
+  ?assertEqual( ?COMMON:get_timestamp( {1421,450191,9061} ), 1421450191 ).
+
+test_parse_binary_ip() ->
+  CorrectIp = <<"127.0.0.1">>,
+  NotIp = <<"Some random value">>,
+  ?assertError(function_clause, ?COMMON:parse_binary_ip( binary_to_list( CorrectIp ) )),
+  ?assertMatch({_, _, _, _}, ?COMMON:parse_binary_ip( CorrectIp )),
+  ?assertMatch({error, {not_ip, _}}, ?COMMON:parse_binary_ip( NotIp )).
+
+test_binary_join() ->
+  Bin1 = <<"A">>,
+  Bin2 = <<"B">>,
+  Bin3 = <<"C">>,
+  ?assertError(undef, ?COMMON:binary_join([ Bin1, Bin2, Bin3 ])),
+  ?assertError(undef, ?COMMON:binary_join(123)),
+  ?assertEqual( <<"ABC">>, ?COMMON:binary_join([ Bin1, Bin2, Bin3 ], <<>>) ),
+  ?assertEqual( <<"A,B,C">>, ?COMMON:binary_join([ Bin1, Bin2, Bin3 ], <<",">>) ).
+
+% Configuration
+
+% Encryption
+
+% Serialization
+
