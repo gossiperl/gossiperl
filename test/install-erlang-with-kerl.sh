ad91a1f3c776debd4f3d847d6202fba72e28618e
radekg <radek@gruchalski.com>
2015-01-25 01:18:15 +0100

    chmod the file

--- test/install-erlang-with-kerl.sh
+++ test/install-erlang-with-kerl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p .erlang
-curl -O https://raw.githubusercontent.com/yrashk/kerl/master/kerl; chmod a+x
+curl -O https://raw.githubusercontent.com/yrashk/kerl/master/kerl
+chmod +x kerl
 ./kerl build 17.0 17.0
 ./kerl install 17.0 .erlang/17.0
 . ./.erlang/17.0/activate
