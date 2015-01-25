6070557523659193e6242010a98815ed4298145c
radekg <radek@gruchalski.com>
2015-01-25 01:40:29 +0100

    Testing clients has to be done with a release.

--- test/install-erlang-with-kerl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p .erlang
-curl -O https://raw.githubusercontent.com/yrashk/kerl/master/kerl
-chmod +x kerl
-./kerl build 17.0 17.0
-./kerl install 17.0 .erlang/17.0
-. ./.erlang/17.0/activate
